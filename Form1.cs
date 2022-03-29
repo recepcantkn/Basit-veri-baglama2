d3ef83389961e36306ff3a37f296f88752ace732
Recep Can <61096668+recepcantkn@users.noreply.github.com>
2022-03-29 09:32:36 +0300

    Basit veri bağlama1

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBagla_Click(object sender, EventArgs e)
+        {
+            Ogrenciler ogrenci = new Ogrenciler();
+            ogrenci.Numara = 1111;
+            ogrenci.AdSoyad = "Mehmet";
+            ogrenci.Alan = "Bilişim Teknolojileri";
+            txtNumara.DataBindings.Add("Text", ogrenci, "Numara");
+            txtAdSoyad.DataBindings.Add("Text", ogrenci, "AdSoyad");
+            txtAlan.DataBindings.Add("Text", ogrenci, "Alan");
+        }
+    }
+}
---
+++ Ogrenciler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication32
+{
+    class Ogrenciler
+    {
+        public int Numara { get; set; }
+        public string AdSoyad { get; set; }
+        public string Alan { get; set; }
+    }
+}
